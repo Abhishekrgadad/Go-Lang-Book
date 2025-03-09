39e6b2e404b7d70a4adec1523127b2a14865a2f8
abhishekrgadad <abhishekgadad485@gmail.com>
2025-03-09 08:43:49 +0530

    Packages in Go

---
+++ Expack/Expack.go
@@ -0,0 +1,12 @@
+package Expack
+
+import (
+	"fmt"
+)
+func init(){
+	fmt.Println("Initializing package ......")
+}
+
+func Mathutil(num int) {
+	fmt.Println(num * num)
+}
---
+++ pack/pack.go
@@ -0,0 +1,7 @@
+package pack
+
+import "fmt"
+
+func Hello(name string){
+	fmt.Println("hello", name)
+}
